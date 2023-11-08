e972a8281fe2841394e552b003b4164acd7c016a
NiyaWi <Niyaw144@gmail.com>
2023-11-08 11:11:48 -0500

    exercise1

---
+++ NewTabs/index.js
@@ -0,0 +1,14 @@
+function googleBtnClick(){
+    console.log("google")
+    window.open("https://www.google.com/?client=safari")
+};
+
+function w3schoolsBtnClick(){
+    console.log("w3sc")
+    window.open("https://www.w3schools.com/js/")
+};
+
+function imagesBtnClick(){
+    console.log("Images")
+    window.open("images.html")
+};
